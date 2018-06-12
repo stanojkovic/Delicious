eea342265ebc1d777893a20090ce07313f4297b3
Саша Станојковић <sasastanojkovic@hotmail.com>
2018-06-12 15:01:07 +0200

    AddMigration
    AddMigration

--- Delicious/Delicious/Models/IdentityModels.cs
+++ Delicious/Delicious/Models/IdentityModels.cs
@@ -9,8 +9,8 @@ namespace Delicious.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public string Ime { get; set; }
-        public string Prezime { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
